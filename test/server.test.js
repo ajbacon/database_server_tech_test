c033b554479d0d39f14702e512c23bf38f5ea361
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-26 17:22:15 +0000

    tidy tests

--- test/server.test.js
+++ test/server.test.js
@@ -1,18 +1,17 @@
 const supertest = require('supertest');
 const session = require('supertest-session');
 const app = require('../server');
+const request = supertest(app);
 
 describe('server', () => {
   describe('GET /', () => {
     it('should return a response status of 200', async () => {
-      const request = supertest(app);
       const response = await request.get('/');
 
       expect(response.status).toEqual(200);
     });
 
     it('should return text of - API Running', async () => {
-      const request = supertest(app);
       const response = await request.get('/');
 
       expect(response.text).toEqual('API Running');
@@ -21,7 +20,6 @@ describe('server', () => {
 
   describe('GET /somethingincorrect', () => {
     it('incorrect route should return a response status of 404', async () => {
-      const request = supertest(app);
       const response = await request.get('/somethingincorrect');
 
       expect(response.status).toEqual(404);
@@ -30,14 +28,12 @@ describe('server', () => {
 
   describe('GET /set', () => {
     it('should return a response status of 200', async () => {
-      const request = supertest(app);
       const response = await request.get('/set');
 
       expect(response.status).toEqual(200);
     });
 
     it('should have have the correct path with query', async () => {
-      const request = supertest(app);
       const response = await request.get('/set?somekey=somevalue');
 
       expect(response.status).toEqual(200);
@@ -47,7 +43,6 @@ describe('server', () => {
 
   describe('GET /get', () => {
     it('should return a response status of 200', async () => {
-      const request = supertest(app);
       const response = await request.get('/get');
 
       expect(response.status).toEqual(200);
@@ -58,10 +53,7 @@ describe('server', () => {
       const response = await testSession
         .get('/set')
         .query({ somekey: 'somedata' });
-
       const response2 = await testSession.get('/get').query({ key: 'somekey' });
-
-      console.log(response2);
 
       expect(response2.body).toEqual('somedata');
     });
