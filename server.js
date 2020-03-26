491270a7d9d66375aca5ebb2ab781b0f2ff26b4a
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-26 13:20:03 +0000

    add /set route

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+app.get('/set', (req, res) => {
+  res.send({ data: req.query });
+});
+
 module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -16,4 +16,20 @@ describe('server', () => {
       expect(response.text).toEqual('API Running');
     });
   });
+
+  describe('GET /somethingincorrect', () => {
+    it('incorrect route should return a response status of 404', async () => {
+      const response = await request.get('/somethingincorrect');
+
+      expect(response.status).toEqual(404);
+    });
+  });
+
+  describe('GET /set?somekey=somevalue', () => {
+    it('should return a response status of 200', async () => {
+      const response = await request.get('/set?somekey=somevalue');
+
+      expect(response.status).toEqual(200);
+    });
+  });
 });
