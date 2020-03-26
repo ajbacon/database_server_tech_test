9c2e709a9ac1cfaa8c717f2180cc96cffba207ac
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-26 11:00:39 +0000

    test for response text of / route

--- test/server.test.js
+++ test/server.test.js
@@ -7,9 +7,13 @@ describe('server', () => {
     it('should return a response status of 200', async () => {
       const response = await request.get('/');
 
-      // console.log(response);
-
       expect(response.status).toEqual(200);
+    });
+
+    it('should return text of - API Running', async () => {
+      const response = await request.get('/');
+
+      expect(response.text).toEqual('API Running');
     });
   });
 });
